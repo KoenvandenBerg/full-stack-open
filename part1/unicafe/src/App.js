e113b01fe85a269134e3342cd019200fe76a9068
Koen van den Berg <kvdbgithub@use.startmail.com>
2022-12-10 12:32:07 +0100

    Exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,9 +6,13 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
   const handleClick = (currentState, setStateFunction) => {
-    return () => setStateFunction(currentState + 1)
+    return () => {
+      setStateFunction(currentState + 1)
+      setTotal(total + 1)
+    }
   }
 
   return (
@@ -21,6 +25,9 @@ const App = () => {
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>total: {total}</p>
+      <p>average: {total === 0 ? "-" : (good * 1 + bad * -1) / total}</p>
+      <p>positive: {total === 0 ? "-" : good / total * 100 + "%"}</p>
     </div>
   )
 }
